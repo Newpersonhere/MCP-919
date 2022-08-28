4d4ff294c20de953a2c1b7f16c5e0fb3c8a8b543
Newpersonhere <97367726+Newpersonhere@users.noreply.github.com>
2022-08-28 16:10:32 -0400

    Update EntityCreeper.java

--- src/minecraft/net/minecraft/entity/monster/EntityCreeper.java
+++ src/minecraft/net/minecraft/entity/monster/EntityCreeper.java
@@ -49,7 +49,7 @@ public class EntityCreeper extends EntityMob
         this.tasks.addTask(5, new EntityAIWander(this, 0.8D));
         this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(6, new EntityAILookIdle(this));
-        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, false));
         this.targetTasks.addTask(2, new EntityAIHurtByTarget(this, false, new Class[0]));
     }
 
@@ -95,7 +95,7 @@ public class EntityCreeper extends EntityMob
 
         if (this.dataWatcher.getWatchableObjectByte(17) == 1)
         {
-            tagCompound.setBoolean("powered", true);
+            tagCompound.setBoolean("powered", false);
         }
 
         tagCompound.setShort("Fuse", (short)this.fuseTime);
